db7d3a8633bb0f4c5a82c49d4962f3c547909a56
Stephan <vanaf1979@gmail.com>
2019-06-26 12:13:01 +0200

    added meta tag output

--- metatags.php
+++ metatags.php
@@ -45,9 +45,9 @@ class MetaTags {
      * @var array $metafields
      */
     private $metafields = array(
-        'metatags_browser_title',
-        'metatags_description_field',
-        'metatags_robots_field'
+        'title' => 'metatags_browser_title',
+        'description' => 'metatags_description_field',
+        'robots' => 'metatags_robots_field'
     );
 
 
@@ -126,6 +126,25 @@ class MetaTags {
             1
         );
 
+        \add_action(
+            'after_setup_theme',
+            array( $this , 'add_theme_support' ),
+            1
+        );
+
+        \add_action(
+            'pre_get_document_title',
+            array( $this , 'add_title_to_head' ),
+            999
+        );
+
+        \add_action(
+            'wp_head',
+            array( $this , 'add_metatags_to_head' ),
+            1,
+            1
+        );
+
     }
 
 
@@ -193,12 +212,83 @@ class MetaTags {
             'type' => 'string',
         );
 
-        foreach( $this->metafields as $metafield ) {
+        foreach( $this->metafields as $key => $value ) {
 
-            \register_meta( 'post' , $metafield , $args );
+            \register_meta( 'post' , $value , $args );
 
         }
         
+    }
+
+
+    /**
+     * add_theme_support.
+     *
+     * Add theme support for the title tag.
+     * 
+     * @uses add_theme_support() https://developer.wordpress.org/reference/functions/add_theme_support/
+     *
+     * @access public
+     * @return void
+     */
+    public function add_theme_support( ) {
+        
+        add_theme_support( 'title-tag' );
+
+    }
+
+
+    /**
+     * add_seabox_title_to_head.
+     *
+     * Add a title tag to the head.
+     *
+     * @uses get_post_meta() https://developer.wordpress.org/reference/functions/get_post_meta/
+     * 
+     * @access public
+     * @param String $title
+     * @return String
+     */
+    public function add_title_to_head( String $title ) : String {
+
+        global $post;
+
+        $title = trim( get_post_meta( $post->ID , 'metatags_browser_title' , true ) );
+
+        return $title > '' ? $title : $post->post_title;
+        
+    }
+
+
+    /**
+     * add_metatags_to_head.
+     *
+     * Add seo meta tags to the head.
+     *
+     * @uses get_post_meta() https://developer.wordpress.org/reference/functions/get_post_meta/
+     * 
+     * @since 1.0.0
+     * @access public
+     * @return void
+     */
+    public function add_metatags_to_head() : void {
+
+        global $post;
+        
+        foreach( $this->metafields as $key => $value ) {
+
+            if ( $key == 'title' ) {
+
+                continue;
+
+            }
+            
+            $value = trim( get_post_meta( $post->ID , $value , true ) );
+
+            echo "\t<meta name=\"{$key}\" content=\"{$value}\" />\n";
+
+        }
+
     }
 
 }
